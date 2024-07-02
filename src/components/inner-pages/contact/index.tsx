f50cbb3754ada18c74fde3100583c85d347f42c1
basheer mulla <basheer1067@gmail.com>
2024-07-02 20:49:28 +0300

    first commit

---
+++ src/components/inner-pages/contact/index.tsx
@@ -0,0 +1,18 @@
+import FooterFour from '@/layouts/footers/FooterFour'
+import HeaderOne from '@/layouts/headers/HeaderOne'
+import HeaderTwo from '@/layouts/headers/HeaderTwo'
+import HeaderThree from '@/layouts/headers/HeaderThree'
+import HeaderFour from '@/layouts/headers/HeaderFour'
+import ContactArea from './ContactArea'
+
+const Contact = () => {
+   return (
+      <>
+         <HeaderOne style={true} />
+         <ContactArea />
+         <FooterFour />
+      </>
+   )
+}
+
+export default Contact
